21cd56fae7dbe5fcd0791f6f472f6aec56e7a330
keiranjprice101 <44777678+keiranjprice101@users.noreply.github.com>
2024-07-03 11:08:33 +0100

    Add new endpoint (#349)
    * Refactor reductions for instrument route
    
    * Add query to get all
    
    * Add missing line of testing

--- fia_api/core/services/reduction.py
+++ fia_api/core/services/reduction.py
@@ -58,6 +58,35 @@ def get_reductions_by_instrument(
     )
 
 
+def get_all_reductions(
+    limit: int = 0,
+    offset: int = 0,
+    order_by: OrderField = "reduction_start",
+    order_direction: Literal["asc", "desc"] = "desc",
+    user_number: int | None = None,
+) -> Sequence[Reduction]:
+    """
+    Get all reductions, if a user number is provided then only the reductions that user has permission for will be
+    provided.
+    :param user_number:  Optional user number to filter with
+    :param limit: (int) - the maximum number of results to be allowed in the sequence
+    :param offset: (int) - the number of reductions to offset the sequence from the entire reduction set
+    :param order_direction: (str) Direction to der by "asc" | "desc"
+    :param order_by: (str) Field to order by.
+    :return: A Sequence of Reductions
+    """
+    if user_number is None:
+        return _REPO.find(
+            ReductionSpecification().all(limit=limit, offset=offset, order_by=order_by, order_direction=order_direction)
+        )
+    experiment_numbers = get_experiments_for_user_number(user_number)
+    return _REPO.find(
+        ReductionSpecification().by_experiment_numbers(
+            experiment_numbers, limit=limit, offset=offset, order_direction=order_direction, order_by=order_by
+        )
+    )
+
+
 def get_reduction_by_id(reduction_id: int, user_number: int | None = None) -> Reduction:
     """
     Given an ID return the reduction with that ID
