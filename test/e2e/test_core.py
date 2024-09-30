958893c1ed75f27e68409f253dee2687fcf03b9c
Dagonite <samuel.stock@stfc.ac.uk>
2024-09-30 09:22:01 +0100

    Update test_core.py

--- test/e2e/test_core.py
+++ test/e2e/test_core.py
@@ -527,7 +527,6 @@ def test_put_instrument_specification_no_api_key():
 
 def test_put_instrument_status():
     """Test instrument status is updated"""
-    client.put("/instrument/mari/status", json={"status": "true"}, headers={"Authorization": "Bearer shh"})
-    response = client.get("/instrument/mari/status", headers={"Authorization": "Bearer shh"})
+    response = client.put("/instrument/mari/status", json={"status": "true"}, headers={"Authorization": "Bearer shh"})
     assert response.status_code == HTTPStatus.OK
-    assert response.json()["enabled"] == "true"
+    assert response.json()["enabled"] is True
