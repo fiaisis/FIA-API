b39800f539d45095f9665af645fa3378d07b5522
Samuel Jones <samuel.jones@stfc.ac.uk>
2024-11-12 15:22:28 +0000

    Quick fix patch

--- fia_api/router.py
+++ fia_api/router.py
@@ -307,7 +307,7 @@ async def get_extras_top_level_folders() -> list[str]:
     return read_dir(root_directory)
 
 
-@ROUTER.get("/extras/{subfolder}", tags=["files"])
+@ROUTER.get("/extras/{subdir}", tags=["files"])
 async def get_subfolder_files_list(subdir: str) -> list[Path]:
     """
     Returns a list of files within a sub_folder. Directs users to use the /extras endpoint if folder not found
