e82212d6259ec8a648340a01901a45f34f72bcd6
Samuel Jones <samuel.jones@stfc.ac.uk>
2024-12-17 13:28:01 +0000

    Turn off mypy for operator and index in loq_transform

--- fia_api/scripts/transforms/loq_transform.py
+++ fia_api/scripts/transforms/loq_transform.py
@@ -15,12 +15,12 @@ from fia_api.scripts.transforms.transform import Transform
 logger = logging.getLogger(__name__)
 
 
+# mypy: disable-error-code="operator, index"
 class LoqTransform(Transform):
     """
     LoqTransform applies modifications to LOQ instrument scripts based on reduction input parameters in a Job
     entity.
     """
-
     def apply(self, script: PreScript, job: Job) -> None:  # noqa: C901
         logger.info("Beginning LOQ transform for job %s...", job.id)
         lines = script.value.splitlines()
@@ -28,47 +28,47 @@ class LoqTransform(Transform):
         # If you get here in the future, try removing the type ignore and see if it passes with newer mypy
         for index, line in enumerate(lines):
             if "/extras/loq/MaskFile.toml" in line and "user_file" in job.inputs:
-                lines[index] = line.replace("/extras/loq/MaskFile.toml", job.inputs["user_file"])  # type: ignore
+                lines[index] = line.replace("/extras/loq/MaskFile.toml", job.inputs["user_file"])
                 continue
             if "run_number" in job.inputs and self._replace_input(
                 line, lines, index, "sample_scatter", job.inputs["run_number"]
-            ):  # type: ignore
+            ):
                 continue
             if "scatter_transmission" in job.inputs and self._replace_input(
                 line, lines, index, "sample_transmission", job.inputs["scatter_transmission"]
-            ):  # type: ignore
+            ):
                 continue
             if "scatter_direct" in job.inputs and self._replace_input(
                 line, lines, index, "sample_direct", job.inputs["scatter_direct"]
-            ):  # type: ignore
+            ):
                 continue
             if "can_scatter" in job.inputs and self._replace_input(
                 line, lines, index, "can_scatter", job.inputs["can_scatter"]
-            ):  # type: ignore
+            ):
                 continue
             if "can_transmission" in job.inputs and self._replace_input(
                 line, lines, index, "can_transmission", job.inputs["can_transmission"]
-            ):  # type: ignore
+            ):
                 continue
             if "can_direct" in job.inputs and self._replace_input(
                 line, lines, index, "can_direct", job.inputs["can_direct"]
-            ):  # type: ignore
+            ):
                 continue
             if "sample_thickness" in job.inputs and self._replace_input(
                 line, lines, index, "sample_thickness", job.inputs["sample_thickness"]
-            ):  # type: ignore
+            ):
                 continue
             if "sample_geometry" in job.inputs and self._replace_input(
                 line, lines, index, "sample_geometry", '"' + job.inputs["sample_geometry"] + '"'
-            ):  # type: ignore
+            ):
                 continue
             if "sample_height" in job.inputs and self._replace_input(
                 line, lines, index, "sample_height", job.inputs["sample_height"]
-            ):  # type: ignore
+            ):
                 continue
             if "sample_width" in job.inputs and self._replace_input(
                 line, lines, index, "sample_width", job.inputs["sample_width"]
-            ):  # type: ignore
+            ):
                 continue
         script.value = "\n".join(lines)
         logger.info("Transform complete for reduction %s", job.id)
