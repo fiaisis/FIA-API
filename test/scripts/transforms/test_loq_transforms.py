8c54eed09c1628ef721ec41a45ad2b48c024e7be
Samuel Jones <samuel.jones@stfc.ac.uk>
2025-01-21 15:40:32 +0000

    Add transform for LOQ setting philimits

--- test/scripts/transforms/test_loq_transforms.py
+++ test/scripts/transforms/test_loq_transforms.py
@@ -39,6 +39,7 @@ sample_geometry = "Square"
 sample_height = 0.0
 sample_width = 0.0
 slice_wavs = [16.0, 2.0, 3.0, 5.0, 9.0],
+phi_limits_list = [(-20, 20), (30, 160)]
 """
     )
 
@@ -63,6 +64,7 @@ def reduction_1():
         "sample_height": 8.0,
         "sample_width": 8.0,
         "slice_wavs": "[1.0, 2.0, 3.0]",
+        "phi_limits_list": "[(-20, 20), (30, 160)]",
     }
     return mock
 
@@ -82,6 +84,7 @@ def reduction_2():
         "sample_height": 8.0,
         "sample_width": 8.0,
         "slice_wavs": "[1.0, 2.0, 3.0]",
+        "phi_limits_list": "[(-20, 20), (30, 160)]",
     }
     return mock
 
@@ -112,6 +115,7 @@ def test_loq_transform_apply(script, reduction_1):
         "sample_height": "sample_height = 8.0",
         "sample_width": "sample_width = 8.0",
         "slice_wavs": "slice_wavs = [1.0, 2.0, 3.0]",
+        "phi_limits": "phi_limits_list = [(-20, 20), (30, 160)]",
     }
 
     for index, line in enumerate(updated_lines):
@@ -149,6 +153,7 @@ def test_loq_transform_apply_with_optionals(script, reduction_2):
         "sample_height": "sample_height = 8.0",
         "sample_width": "sample_width = 8.0",
         "slice_wavs": "slice_wavs = [1.0, 2.0, 3.0]",
+        "phi_limits": "phi_limits_list = [(-20, 20), (30, 160)]",
     }
 
     for index, line in enumerate(updated_lines):
