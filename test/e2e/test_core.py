d13af7f1b224f6d5410980303289b247c2bba21f
Dagonite <samuel.stock@stfc.ac.uk>
2025-02-05 08:23:38 +0000

    Remove tests

--- test/e2e/test_core.py
+++ test/e2e/test_core.py
@@ -555,51 +555,6 @@ def test_get_instrument_jobs_as_user_dev_mode(mock_get_experiment_numbers_for_us
         assert len(response.json()) == expected_number_of_jobs
 
 
-@patch("fia_api.core.auth.tokens.requests.post")
-def test_get_jobs_as_user_on_and_user(mock_post):
-    """Test get all jobs with as_user=True for a user"""
-    mock_post.return_value.status_code = HTTPStatus.OK
-    response = client.get("/jobs?as_user=true", headers={"Authorization": f"Bearer {USER_TOKEN}"})
-    assert response.status_code == HTTPStatus.OK
-    assert len(response.json()) > 0
-
-
-@patch("fia_api.core.auth.tokens.requests.post")
-def test_get_jobs_as_user_off_and_user(mock_post):
-    """Test get all jobs with as_user=False for a user"""
-    mock_post.return_value.status_code = HTTPStatus.OK
-    response = client.get("/jobs?as_user=false", headers={"Authorization": f"Bearer {USER_TOKEN}"})
-    assert response.status_code == HTTPStatus.OK
-    assert len(response.json()) > 0
-
-
-@patch("fia_api.core.auth.tokens.requests.post")
-def test_get_jobs_by_instrument_as_user_on_and_staff(mock_post):
-    """Test get instrument jobs with as_user=True for a staff"""
-    mock_post.return_value.status_code = HTTPStatus.OK
-    response = client.get("/instrument/mari/jobs?as_user=true", headers={"Authorization": f"Bearer {STAFF_TOKEN}"})
-    assert response.status_code == HTTPStatus.OK
-    assert len(response.json()) > 0
-
-
-@patch("fia_api.core.auth.tokens.requests.post")
-def test_get_jobs_by_instrument_as_user_on_and_user(mock_post):
-    """Test get instrument jobs with as_user=True for a user"""
-    mock_post.return_value.status_code = HTTPStatus.OK
-    response = client.get("/instrument/mari/jobs?as_user=true", headers={"Authorization": f"Bearer {USER_TOKEN}"})
-    assert response.status_code == HTTPStatus.OK
-    assert len(response.json()) > 0
-
-
-@patch("fia_api.core.auth.tokens.requests.post")
-def test_get_jobs_by_instrument_as_user_off_and_user(mock_post):
-    """Test get instrument jobs with as_user=False for a user"""
-    mock_post.return_value.status_code = HTTPStatus.OK
-    response = client.get("/instrument/mari/jobs?as_user=false", headers={"Authorization": f"Bearer {USER_TOKEN}"})
-    assert response.status_code == HTTPStatus.OK
-    assert len(response.json()) > 0
-
-
 def test_instrument_jobs_count():
     """
     Test instrument jobs count
