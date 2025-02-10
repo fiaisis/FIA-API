757e70acd53d6fb348230533adcb9df017819e84
Dagonite <samuel.stock@stfc.ac.uk>
2025-02-10 10:38:40 +0000

    Update test_core.py

--- test/e2e/test_core.py
+++ test/e2e/test_core.py
@@ -661,9 +661,8 @@ def test_get_mantid_runners_bad_jwt(mock_post):
 
 
 @patch("fia_api.core.auth.tokens.requests.post")
-@patch("fia_api.core.services.job.get_all_jobs")
 @patch("fia_api.core.services.job.get_experiments_for_user_number")
-def test_get_jobs_as_user_flag_for_staff(mock_post, mock_get_all_jobs, mock_get_experiment_numbers_for_user_number):
+def test_get_jobs_as_user_flag_for_staff(mock_post, mock_get_experiment_numbers_for_user_number):
     """Test get all jobs with as_user flag set to true and false for a staff user"""
 
     x = 1
