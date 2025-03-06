f77c55117346fcfb72f469da9217050ac30b1d6d
Samuel Jones <samuel.jones@stfc.ac.uk>
2025-03-06 15:29:46 +0000

    Add vesuvio transforms

--- fia_api/scripts/transforms/factory.py
+++ fia_api/scripts/transforms/factory.py
@@ -9,6 +9,7 @@ from fia_api.scripts.transforms.sans_transform import SansTransform
 from fia_api.scripts.transforms.test_transforms import TestTransform
 from fia_api.scripts.transforms.tosca_transform import ToscaTransform
 from fia_api.scripts.transforms.transform import MissingTransformError, Transform
+from fia_api.scripts.transforms.vesuvio_transform import VesuvioTransform
 
 logger = logging.getLogger(__name__)
 
@@ -31,6 +32,8 @@ def get_transform_for_instrument(instrument: str) -> Transform:
             return SansTransform()
         case "iris":
             return IrisTransform()
+        case "vesuvio":
+            return VesuvioTransform()
         case "test":
             return TestTransform()
         case _:
---
+++ fia_api/scripts/transforms/vesuvio_transform.py
@@ -0,0 +1,50 @@
+"""
+Module provides the VesuvioTransform class, an implementation of the Transform abstract base class for VESUVIO instrument
+scripts.
+"""
+
+import logging
+
+from db.data_models import Job
+from sqlalchemy import ColumnElement
+from sqlalchemy.dialects.postgresql import JSONB
+
+from fia_api.scripts.pre_script import PreScript
+from fia_api.scripts.transforms.transform import Transform
+
+logger = logging.getLogger(__name__)
+
+
+class VesuvioTransform(Transform):
+    """
+    VesuvioTransform applies modifications to VESUVIO instrument scripts based on reduction input parameters in a Job
+    entity.
+    """
+
+    def apply(self, script: PreScript, job: Job) -> None:
+        logger.info("Beginning Vesuvio transform for job %s...", job.id)
+        lines = script.value.splitlines()
+        # MyPY does not believe ColumnElement[JSONB] is indexable, despite JSONB implementing the Indexable mixin
+        # If you get here in the future, try removing the type ignore and see if it passes with newer mypy
+        for index, line in enumerate(lines):
+            if self._replace_input(line, lines, index, "ip", f"\"{job.inputs["ip_file"]}\""):  # type: ignore
+                continue
+            if self._replace_input(line, lines, index, "runno", f"\"{job.inputs["runno"]}\""):  # type: ignore
+                continue
+            if self._replace_input(line, lines, index, "empty_runs", f"\"{job.inputs["empty_runs"]}\""):  # type: ignore
+                continue
+        script.value = "\n".join(lines)
+        logger.info("Transform complete for reduction %s", job.id)
+
+    @staticmethod
+    def _replace_input(
+        line: str,
+        lines: list[str],
+        index: int,
+        line_start: str,
+        replacement: ColumnElement["JSONB"],
+    ) -> bool:
+        if line.startswith(line_start):
+            lines[index] = f"{line_start} = {replacement}"
+            return True
+        return False
