a50323b217a79c60e71cdd43eca3212f30eba899
Dagonite <samuel.stock@stfc.ac.uk>
2025-03-14 12:58:44 +0000

    Fix prefix

--- fia_api/routers/download_file.py
+++ fia_api/routers/download_file.py
@@ -7,7 +7,7 @@ from fastapi.responses import FileResponse
 from fia_api.core.auth.tokens import JWTAPIBearer
 
 ceph_dir = os.environ.get("CEPH_DIR", "/ceph")
-download_file_router = APIRouter(prefix=f"/{ceph_dir}", tags=["download_files"])
+download_file_router = APIRouter(prefix=ceph_dir, tags=["download_files"])
 jwt_api_security = JWTAPIBearer()
 
 
