a89aa0b84f6f582f78caa2772490170fb847e7c9
Keiran Price <keiran.price@stfc.ac.uk>
2025-03-24 10:33:35 +0000

    Add job request exception handler

--- fia_api/fia_api.py
+++ fia_api/fia_api.py
@@ -8,12 +8,14 @@ from starlette.middleware.cors import CORSMiddleware
 
 from fia_api.core.exceptions import (
     AuthenticationError,
+    JobRequestError,
     MissingRecordError,
     MissingScriptError,
     UnsafePathError,
 )
 from fia_api.exception_handlers import (
     authentication_error_handler,
+    bad_job_request_handler,
     missing_record_handler,
     missing_script_handler,
     unsafe_path_handler,
@@ -60,3 +62,4 @@ app.add_exception_handler(MissingRecordError, missing_record_handler)
 app.add_exception_handler(MissingScriptError, missing_script_handler)
 app.add_exception_handler(UnsafePathError, unsafe_path_handler)
 app.add_exception_handler(AuthenticationError, authentication_error_handler)
+app.add_exception_handler(JobRequestError, bad_job_request_handler)
