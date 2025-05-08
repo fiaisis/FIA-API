4d6688714d1072b6dd37f82bcc245c80aeca4fb4
Keiran Price <keiran.price@stfc.ac.uk>
2025-05-08 07:31:29 +0100

    Set instrument manually in both cases

--- fia_api/core/services/job.py
+++ fia_api/core/services/job.py
@@ -241,6 +241,7 @@ def create_autoreduction_job(job_request: AutoreductionRequest) -> Job:
             run_id=run.id,
             owner_id=run.owner_id,
             instrument_id=run.instrument_id,
+            instrument=run.instrument,
         )
         instrument = run.instrument
 
