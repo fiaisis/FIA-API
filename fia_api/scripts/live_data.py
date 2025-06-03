7b585cf55b37446e55fa121dd1f25dbd59365f1b
Keiran Price <keiran.price@stfc.ac.uk>
2025-06-03 11:53:08 +0100

    Refactor GitHub auth handling and add live data script API.

--- fia_api/scripts/live_data.py
+++ fia_api/scripts/live_data.py
@@ -1,22 +1,29 @@
 import os
+from typing import cast
 
-from github import Github
+from github import Auth, Github
+from github.ContentFile import ContentFile
 
 from fia_api.core.models import InstrumentString
 
 TOKEN = os.environ.get("SCRIPT_WRITE_TOKEN", "shh")
 
-GITHUB = Github(TOKEN)
-script_repo = GITHUB.get_repo("fiaisis/autoreduction-scripts")
-
 
 class LiveDataScript:
     def __init__(self, instrument: InstrumentString) -> None:
+        # We create the repo instance inside the init to prevent pygithub from attempting gh connection on import
+        auth = Auth.Token(token=TOKEN)
+        github = Github(auth=auth)
+        self._script_repo = github.get_repo("fiaisis/autoreduction-scripts")
+
         self.instrument = instrument
         self.value, self._file_sha = self._fetch_latest_script()
 
     def _fetch_latest_script(self) -> tuple[str, str]:
-        file = script_repo.get_contents(f"{self.instrument.upper()}/live_data.py")
+        file = cast(
+            ContentFile,  # noqa: TC006, remove at own peril
+            self._script_repo.get_contents(f"{self.instrument.upper()}/live_data.py"),
+        )  # safe cast as getting file not dir
         return file.decoded_content.decode(), file.sha
 
     def update(self, value: str) -> None:
@@ -24,7 +31,7 @@ class LiveDataScript:
         Update the script in the remote repository
         :return: None
         """
-        script_repo.update_file(
+        self._script_repo.update_file(
             f"{self.instrument.upper()}/live_data.py",
             f"FIA-API triggered {self.instrument} live data script update",
             value,
