6e4da8f11f1146142b3e6d30d0fe1c3729865632
esmithExperimentControls <evan.smith@stfc.ac.uk>
2025-10-06 13:20:57 +0100

    Add new transform to add json output onto end of scripts for each transform

--- fia_api/scripts/transforms/factory.py
+++ fia_api/scripts/transforms/factory.py
@@ -6,6 +6,7 @@ from fia_api.scripts.transforms.enginx_transform import EnginxTransform
 from fia_api.scripts.transforms.iris_transform import IrisTransform
 from fia_api.scripts.transforms.mari_transforms import MariTransform
 from fia_api.scripts.transforms.osiris_transform import OsirisTransform
+from fia_api.scripts.transforms.output_transform import OutputTransform
 from fia_api.scripts.transforms.sans_transform import SansTransform
 from fia_api.scripts.transforms.test_transforms import TestTransform
 from fia_api.scripts.transforms.tosca_transform import ToscaTransform
@@ -37,6 +38,8 @@ def get_transform_for_instrument(instrument: str) -> Transform:  # noqa: PLR0911
             return VesuvioTransform()
         case "enginx":
             return EnginxTransform()
+        case "output":
+            return OutputTransform()
         case "test":
             return TestTransform()
         case _:
---
+++ fia_api/scripts/transforms/output_transform.py
@@ -0,0 +1,17 @@
+from fia_api.scripts.transforms.transform import Transform
+
+
+class OutputTransform(Transform):
+    def apply_json_output(self, script: str) -> str:
+        """
+        The aim is to force whatever the script that is passed to also output to stdinput a json string that consists of
+        3 values, status of the run (status), status message, and output files.
+        :return: The passed script with 3 lines added to ensure a json dump occurs at the end
+        """
+        script_addon = (
+            "import json\n"
+            "\n"
+            "print(json.dumps({'status': 'Successful', 'status_message':"
+            "'','output_files': output, 'stacktrace': ''}))\n"
+        )
+        return script + "\n" + script_addon
