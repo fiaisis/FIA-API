cb62a96c1a57b7ad8e8874e9abeb1e2c1cb34d77
esmithExperimentControls <evan.smith@stfc.ac.uk>
2025-10-06 14:23:37 +0100

    Add test for output_transform

--- test/scripts/transforms/test_output_transforms.py
+++ test/scripts/transforms/test_output_transforms.py
@@ -1,1 +1,23 @@
 """Tests for output_transforms.py"""
+
+from unittest.mock import Mock
+from fia_api.scripts.pre_script import PreScript
+from fia_api.scripts.transforms.output_transform import OutputTransform
+
+SCRIPT = """# import os
+print(hello, world)
+"""
+
+def test_output_transform(script):
+  """
+  """
+  script_addon = (
+            "import json\n"
+            "\n"
+            "print(json.dumps({'status': 'Successful', 'status_message':"
+            "'','output_files': output, 'stacktrace': ''}))\n"
+        )
+  script = PreScript(value=SCRIPT)
+  OutputTransform().apply(script, Mock())
+
+  assert script.value.endswith(script_addon)
