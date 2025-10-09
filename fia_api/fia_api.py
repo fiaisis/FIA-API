972698f222a9a5976728a4be32864c73f94a4b0f
Sam <samjones714@gmail.com>
2025-10-09 15:05:47 +0100

    Fix an issue with the opentelemetry tracing

--- fia_api/fia_api.py
+++ fia_api/fia_api.py
@@ -9,7 +9,7 @@ from fastapi.exceptions import RequestValidationError
 from starlette.middleware.cors import CORSMiddleware
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
@@ -69,7 +69,7 @@ tracer = trace.get_tracer(__name__)
 
 # Set up OTLP exporter
 otlp_exporter = OTLPSpanExporter(endpoint=OTEL_EXPORTER_OTLP_ENDPOINT)
-span_processor = BatchExportSpanProcessor(otlp_exporter)
+span_processor = BatchSpanProcessor(otlp_exporter)
 trace.get_tracer_provider().add_span_processor(span_processor)
 
 app = FastAPI(title="FIA API", root_path="/" if DEV_MODE else "/api")
