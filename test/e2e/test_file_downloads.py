e20db74fb21abd902ff5537cc86e2e010d97a45d
esmithExperimentControls <evan.smith@stfc.ac.uk>
2025-11-25 11:04:43 +0000

    Add HTTPStatus assertion

--- test/e2e/test_file_downloads.py
+++ test/e2e/test_file_downloads.py
@@ -74,7 +74,8 @@ def test_download_file_no_owner(mock_post, mock_get_experiments, mock_get_job):
 
     with pytest.raises(JobOwnerError):
         response = client.get("/job/5001/filename/test.nxspe", headers=STAFF_HEADER)
-
+    
+    assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
     assert pytest.raises(JobOwnerError)
 
 
