8435619bc117b8e28cf69040ca85430ccc561c64
esmithExperimentControls <evan.smith@stfc.ac.uk>
2025-12-12 16:25:24 +0000

    Create new file for db session

---
+++ fia_api/core/session.py
@@ -0,0 +1,22 @@
+import os
+
+from sqlalchemy import NullPool, create_engine
+from sqlalchemy.orm import sessionmaker
+
+DB_USERNAME = os.environ.get("DB_USERNAME", "postgres")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "password")
+DB_IP = os.environ.get("DB_IP", "localhost")
+
+ENGINE = create_engine(
+    f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_IP}:5432/fia",
+    poolclass=NullPool,
+)
+
+SESSION = sessionmaker(ENGINE)
+
+
+async def create_session():
+    try:
+        yield SESSION
+    finally:
+        SESSION.close()
