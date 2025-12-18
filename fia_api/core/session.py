bf53569153bf1e25d6ebbd872acf09b1596588fb
esmithExperimentControls <evan.smith@stfc.ac.uk>
2025-12-18 09:46:02 +0000

    Add session.py dependency

--- fia_api/core/session.py
+++ fia_api/core/session.py
@@ -1,31 +1,25 @@
 import os
-from typing import Generator
+from collections.abc import Generator
 
-from sqlalchemy import NullPool, create_engine 
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import NullPool, create_engine
+from sqlalchemy.orm import Session, sessionmaker
 
 DB_USERNAME = os.environ.get("DB_USERNAME", "postgres")
 DB_PASSWORD = os.environ.get("DB_PASSWORD", "password")
 DB_IP = os.environ.get("DB_IP", "localhost")
-DB_URL = (
-    f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_IP}:5432/fia"
-)
+DB_URL = f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_IP}:5432/fia"
 
 ENGINE = create_engine(
     DB_URL,
     poolclass=NullPool,
 )
 
-SessionLocal = sessionmaker(
-    bind=ENGINE,
-    autoflush=False,
-    autocommit=False
-)
+SessionLocal = sessionmaker(bind=ENGINE, autoflush=False, autocommit=False)
 
 
 def get_db_session() -> Generator[Session, None, None]:
     db = SessionLocal()
-      try:
-          yield db
-      finally:
-          db.close()
+    try:
+        yield db
+    finally:
+        db.close()
