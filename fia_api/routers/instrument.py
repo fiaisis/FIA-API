da77cabc72eee11933c4ad3d9e7c09c1bcd22d6f
esmithExperimentControls <evan.smith@stfc.ac.uk>
2025-12-18 09:42:40 +0000

    add session DI to instrument.py

--- fia_api/routers/instrument.py
+++ fia_api/routers/instrument.py
@@ -2,12 +2,12 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 from fastapi.security import HTTPAuthorizationCredentials
+from sqlalchemy.orm import Session
 
 from fia_api.core.auth.tokens import JWTAPIBearer, get_user_from_token
 from fia_api.core.exceptions import AuthError
 from fia_api.core.services.instrument import get_latest_run_by_instrument_name, update_latest_run_for_instrument
 from fia_api.core.session import get_db_session
-from sqlalchemy.orm import Session
 
 InstrumentRouter = APIRouter(prefix="/instrument")
 jwt_api_security = JWTAPIBearer()
@@ -15,19 +15,22 @@ jwt_api_security = JWTAPIBearer()
 
 @InstrumentRouter.get("/{instrument}/latest-run", tags=["instrument"])
 async def get_instrument_latest_run(
-    instrument: str, credentials: Annotated[HTTPAuthorizationCredentials, Depends(jwt_api_security)], db: Session = Depends(get_db_session)
+    instrument: str,
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(jwt_api_security)],
+    session: Annotated[Session, Depends(get_db_session)],
 ) -> dict[str, str | None]:
     """
     Return the latest run for the given instrument
     \f
     :param instrument: The instrument name
+    :param session: The current session of the request
     :return: The latest run
     """
     user = get_user_from_token(credentials.credentials)
     if user.role != "staff":
         # If not staff this is not allowed
         raise AuthError("User not authorised for this action")
-    latest_run = get_latest_run_by_instrument_name(instrument.upper(), db)
+    latest_run = get_latest_run_by_instrument_name(instrument.upper(), session)
     return {"latest_run": latest_run}
 
 
@@ -36,18 +39,20 @@ async def update_instrument_latest_run(
     instrument: str,
     latest_run: dict[str, str],
     credentials: Annotated[HTTPAuthorizationCredentials, Depends(jwt_api_security)],
-    db: Session = Depends(get_db_session)
+    session: Annotated[Session, Depends(get_db_session)],
 ) -> dict[str, str]:
     """
     Update the latest run for the given instrument
     \f
     :param instrument: The instrument name
     :param latest_run: The latest run as a JSON object with a 'latest_run' field
+    :param credentials: Dependency Injected credentials
+    :param session: The current session of the request
     :return: The updated latest run
     """
     user = get_user_from_token(credentials.credentials)
     if user.role != "staff":
         # If not staff this is not allowed
         raise AuthError("User not authorised for this action")
-    update_latest_run_for_instrument(instrument.upper(), latest_run["latest_run"], db)
+    update_latest_run_for_instrument(instrument.upper(), latest_run["latest_run"], session)
     return {"latest_run": latest_run["latest_run"]}
