35d7fd60cb1421902af42d307d2c26d683405a3a
esmithExperimentControls <evan.smith@stfc.ac.uk>
2026-01-08 08:57:26 +0000

    Fix mocking for test_instrument.py

--- test/core/services/test_instrument.py
+++ test/core/services/test_instrument.py
@@ -45,110 +45,140 @@ def test_get_specification_by_instrument_name_instrument_missing(mock_repo):
             get_specification_by_instrument_name(instrument_name="mari", session=mock_session)
 
 
-@patch("fia_api.core.services.instrument._REPO")
+@patch("fia_api.core.services.instrument.Repo")
 @patch("fia_api.core.services.instrument.InstrumentSpecification")
 def test_update_specification_for_instrument(mock_spec, mock_repo):
+    mock_session = Mock()
+    mock_repo_instance = Mock()
+    mock_repo.return_value = mock_repo_instance
     instrument = Mock()
 
-    mock_repo.find_one.return_value = instrument
+    mock_repo_instance.find_one.return_value = instrument
 
-    update_specification_for_instrument("mari", {"foo": 1})
+    update_specification_for_instrument("mari", {"foo": 1}, mock_session)
 
-    mock_repo.find_one.assert_called_once_with(mock_spec.return_value.by_name.return_value)
+    mock_repo_instance.find_one.assert_called_once_with(mock_spec.return_value.by_name.return_value)
     assert instrument.specification == {"foo": 1}
-    mock_repo.update_one.assert_called_once_with(instrument)
+    mock_repo_instance.update_one.assert_called_once_with(instrument)
 
 
-@patch("fia_api.core.services.instrument._REPO")
+@patch("fia_api.core.services.instrument.Repo")
 def test_update_specification_for_instrument_instrument_missing(mock_repo):
+    mock_session = Mock()
+    mock_repo_instance = Mock()
+    mock_repo.return_value = mock_repo_instance
     with patch("fia_api.core.services.instrument.InstrumentSpecification"):
-        mock_repo.find_one.return_value = None
+        mock_repo_instance.find_one.return_value = None
         with pytest.raises(MissingRecordError):
-            update_specification_for_instrument("mari", {"foo": 1})
+            update_specification_for_instrument("mari", {"foo": 1}, mock_session)
 
 
-@patch("fia_api.core.services.instrument._REPO")
+@patch("fia_api.core.services.instrument.Repo")
 @patch("fia_api.core.services.instrument.InstrumentSpecification")
 def test_get_latest_run_by_instrument_name(mock_spec, mock_repo):
+    mock_session = Mock()
+    mock_repo_instance = Mock()
+    mock_repo.return_value = mock_repo_instance
     mock_instrument = Mock()
-    mock_repo.find_one.return_value = mock_instrument
+    mock_repo_instance.find_one.return_value = mock_instrument
 
-    latest_run = get_latest_run_by_instrument_name("mari")
+    latest_run = get_latest_run_by_instrument_name("mari", mock_session)
 
     assert latest_run == mock_instrument.latest_run
     mock_spec.return_value.by_name.assert_called_once_with("mari")
 
 
-@patch("fia_api.core.services.instrument._REPO")
+@patch("fia_api.core.services.instrument.Repo")
 def test_get_latest_run_by_instrument_name_instrument_missing(mock_repo):
+    mock_session = Mock()
+    mock_repo_instance = Mock()
+    mock_repo.return_value = mock_repo_instance
     with patch("fia_api.core.services.instrument.InstrumentSpecification"):
-        mock_repo.find_one.return_value = None
+        mock_repo_instance.find_one.return_value = None
 
         with pytest.raises(MissingRecordError):
-            get_latest_run_by_instrument_name("mari")
+            get_latest_run_by_instrument_name("mari", mock_session)
 
 
-@patch("fia_api.core.services.instrument._REPO")
+@patch("fia_api.core.services.instrument.Repo")
 @patch("fia_api.core.services.instrument.InstrumentSpecification")
 def test_update_latest_run_for_instrument(mock_spec, mock_repo):
+    mock_session = Mock()
+    mock_repo_instance = Mock()
+    mock_repo.return_value = mock_repo_instance
     instrument = Mock()
 
-    mock_repo.find_one.return_value = instrument
+    mock_repo_instance.find_one.return_value = instrument
 
-    update_latest_run_for_instrument("mari", "MARI12345")
+    update_latest_run_for_instrument("mari", "MARI12345", mock_session)
 
-    mock_repo.find_one.assert_called_once_with(mock_spec.return_value.by_name.return_value)
+    mock_repo_instance.find_one.assert_called_once_with(mock_spec.return_value.by_name.return_value)
     assert instrument.latest_run == "MARI12345"
-    mock_repo.update_one.assert_called_once_with(instrument)
+    mock_repo_instance.update_one.assert_called_once_with(instrument)
 
 
-@patch("fia_api.core.services.instrument._REPO")
+@patch("fia_api.core.services.instrument.Repo")
 def test_update_latest_run_for_instrument_instrument_missing(mock_repo):
+    mock_session = Mock()
+    mock_repo_instance = Mock()
+    mock_repo.return_value = mock_repo_instance
     with patch("fia_api.core.services.instrument.InstrumentSpecification"):
-        mock_repo.find_one.return_value = None
+        mock_repo_instance.find_one.return_value = None
         with pytest.raises(MissingRecordError):
-            update_latest_run_for_instrument("mari", "MARI12345")
+            update_latest_run_for_instrument("mari", "MARI12345", mock_session)
 
 
-@patch("fia_api.core.services.instrument._REPO")
+@patch("fia_api.core.services.instrument.Repo")
 @patch("fia_api.core.services.instrument.InstrumentSpecification")
 def test_get_live_data_script_by_instrument_name(mock_spec, mock_repo):
+    mock_session = Mock()
+    mock_repo_instance = Mock()
+    mock_repo.return_value = mock_repo_instance
     mock_instrument = Mock()
     mock_instrument.live_data_script = "print('hello')"
-    mock_repo.find_one.return_value = mock_instrument
+    mock_repo_instance.find_one.return_value = mock_instrument
 
-    script = get_live_data_script_by_instrument_name("mari")
+    script = get_live_data_script_by_instrument_name("mari", mock_session)
 
     assert script == mock_instrument.live_data_script
     mock_spec.return_value.by_name.assert_called_once_with("mari")
 
 
-@patch("fia_api.core.services.instrument._REPO")
+@patch("fia_api.core.services.instrument.Repo")
 def test_get_live_data_script_by_instrument_name_instrument_missing(mock_repo):
+    mock_session = Mock()
+    mock_repo_instance = Mock()
+    mock_repo.return_value = mock_repo_instance
     with patch("fia_api.core.services.instrument.InstrumentSpecification"):
-        mock_repo.find_one.return_value = None
+        mock_repo_instance.find_one.return_value = None
 
         with pytest.raises(MissingRecordError):
-            get_live_data_script_by_instrument_name("mari")
+            get_live_data_script_by_instrument_name("mari", mock_session)
 
 
-@patch("fia_api.core.services.instrument._REPO")
+@patch("fia_api.core.services.instrument.Repo")
 @patch("fia_api.core.services.instrument.InstrumentSpecification")
 def test_update_live_data_script_for_instrument(mock_spec, mock_repo):
+    mock_session = Mock()
+    mock_repo_instance = Mock()
+    mock_repo.return_value = mock_repo_instance
     instrument = Mock()
 
-    mock_repo.find_one.return_value = instrument
+    mock_repo_instance.find_one.return_value = instrument
 
-    update_live_data_script_for_instrument("mari", "print('hello world')")
+    update_live_data_script_for_instrument("mari", "print('hello world')", mock_session)
 
-    mock_repo.find_one.assert_called_once_with(mock_spec.return_value.by_name.return_value)
+    mock_repo_instance.find_one.assert_called_once_with(mock_spec.return_value.by_name.return_value)
     assert instrument.live_data_script == "print('hello world')"
-    mock_repo.update_one.assert_called_once_with(instrument)
+    mock_repo_instance.update_one.assert_called_once_with(instrument)
 
 
-@patch("fia_api.core.services.instrument._REPO")
+@patch("fia_api.core.services.instrument.Repo")
 def test_update_live_data_script_for_instrument_instrument_missing(mock_repo):
+    mock_session = Mock()
+    mock_repo_instance = Mock()
+    mock_repo.return_value = mock_repo_instance
     with patch("fia_api.core.services.instrument.InstrumentSpecification"):
-        mock_repo.find_one.return_value = None
+        mock_repo_instance.find_one.return_value = None
         with pytest.raises(MissingRecordError):
-            update_live_data_script_for_instrument("mari", "print('hello world')")
+            update_live_data_script_for_instrument("mari", "print('hello world')", mock_session)
