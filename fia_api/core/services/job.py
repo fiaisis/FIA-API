f7fba5b928f9c153103c3d38cd14fe910a93310e
esmithExperimentControls <evan.smith@stfc.ac.uk>
2026-01-19 13:34:31 +0000

    changing type hint for job_maker method in job.py service

--- fia_api/core/services/job.py
+++ fia_api/core/services/job.py
@@ -33,7 +33,7 @@ from fia_api.core.utility import (
 from fia_api.scripts.acquisition import get_script_for_job
 
 
-def job_maker(session: Session) -> JobMaker:
+def job_maker(session: Annotated[Session, Depends(get_db_session)]) -> JobMaker:
     """Creates a JobMaker and returns it using env vars"""
     queue_host = os.environ.get("QUEUE_HOST", "localhost")
     queue_name = os.environ.get("EGRESS_QUEUE_NAME", "scheduled-jobs")
