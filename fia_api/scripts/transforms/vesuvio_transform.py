3e4cf2bea2dd4c3c2cd8416823aa9752a76d21b8
esmithExperimentControls <evan.smith@stfc.ac.uk>
2026-01-28 09:00:57 +0000

    Changing FIA-API vesuvio script to allow summing multiple runs

--- fia_api/scripts/transforms/vesuvio_transform.py
+++ fia_api/scripts/transforms/vesuvio_transform.py
@@ -24,12 +24,24 @@ class VesuvioTransform(Transform):
     def apply(self, script: PreScript, job: Job) -> None:
         logger.info("Beginning Vesuvio transform for job %s...", job.id)
         lines = script.value.splitlines()
-        # MyPY does not believe ColumnElement[JSONB] is indexable, despite JSONB implementing the Indexable mixin
-        # If you get here in the future, try removing the type ignore and see if it passes with newer mypy
+
+        runno = job.inputs["runno"]
+        if isinstance(runno, list):
+            if len(runno) > 1:
+                # Convert list to range string if contiguous, otherwise comma-separated
+                if all(runno[i] == runno[i-1] + 1 for i in range(1, len(runno))):
+                    runno_str = f"{runno[0]}-{runno[-1]}"
+                else:
+                    runno_str = ",".join(map(str, runno))
+            else:
+                runno_str = str(runno[0])
+        else:
+            runno_str = str(runno)
+
         for index, line in enumerate(lines):
             if self._replace_input(line, lines, index, "ip", f'"{job.inputs["ip_file"]}"'):  # type: ignore
                 continue
-            if self._replace_input(line, lines, index, "runno", f'"{job.inputs["runno"]}"'):  # type: ignore
+            if self._replace_input(line, lines, index, "runno", f'"{runno_str}"'):  # type: ignore
                 continue
             if self._replace_input(line, lines, index, "empty_runs", f'"{job.inputs["empty_runs"]}"'):  # type: ignore
                 continue
